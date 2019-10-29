30138d476bf208730b027d63e05b91601431ecce
Lindsey Cason <lindseyacason@gmail.com>
2019-10-29 12:59:16 -0500

    update

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,22 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+      console.log("colorlist", colorList)
+
+  useEffect(()=>{
+    axiosWithAuth()
+    .get("/colors")
+    .then(res =>{
+      console.log("response from use effect bubblespage", res.data);
+      setColorList(res.data)
+    })
+  }, [])//need dep array to stop loop.
 
   return (
     <>
